a5a3b0d5c48539ce4d8678f8f116fadf52296546
Austin Juntra <AustinJuntra@gmail.com>
2015-01-07 08:49:20 -0800

    class fix and continue

--- lesson1.php
+++ lesson1.php
@@ -17,3 +17,45 @@ $cat1->firstname = "Tom";
 $cat1->lastname = "Baret";
 print "The Cat's name is {$cat1->getName()}.";
 
+////////////
+
+class Bat {
+
+    public $firstname = “default firstname”;
+ 	public $lastname = “default lastname”;
+ 	public $gender = “male”;
+ 	public $price = 0;
+
+function getName(){
+	return "{$this->firstname}" .
+	"{this->lastname}";
+}
+}
+
+$bat1 = new Bat();
+$bat1->firstname = "Bat";
+$bat1->lastname = "Man";
+print "The Bat's name is {$Bat1->getName()}.";
+
+///////////
+
+class Person {
+
+    public $firstname = “default firstname”;
+ 	public $lastname = “default lastname”;
+ 	public $gender = “male”;
+ 	public $price = 0;
+
+function getName(){
+	return "{$this->firstname}" .
+	"{this->lastname}";
+}
+}
+
+$person1 = new Person();
+$person1->firstname = "Iron";
+$person1->lastname = "Man";
+print "The Person's name is {$person1->getName()}.";
+
+
+
