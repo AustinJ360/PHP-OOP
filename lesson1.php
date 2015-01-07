e4c3989340e7edb48008083b0813bc5db76006d5
Austin Juntra <AustinJuntra@gmail.com>
2015-01-06 21:08:25 -0800

    class continue

--- lesson1.php
+++ lesson1.php
@@ -1,6 +1,18 @@
-class Car {
-	class mustang
+class Cat {
+
+    public $firstname = “default firstname”;
+ 	public $lastname = “default lastname”;
+ 	public $gender = “male”;
+ 	public $price = 0;
+
+function getName(){
+	return "{$this->firstname}" .
+	"{this->lastname}";
+}
 }
 
-$mustang = new car();
-$dodge = new car();
+$cat1 = new Cat();
+$cat1->firstname = "Tom";
+$cat1->lastname = "Baret";
+print "The Cat's name is {$cat1->getName()}.";
+
