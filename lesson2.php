98aab0edb77a8bd3e28012b436c91e10db45928b
Austin Juntra <AustinJuntra@gmail.com>
2015-01-07 19:35:11 -0800

    lesson 2

--- lesson2.php
+++ lesson2.php
@@ -1,20 +1,61 @@
 <?php
-class Dog {
-	public $firstName;
-	public $lastName;
-	public $breed;
+class Person {
+	public $firstName = "Austin";
+	public $lastName = "Juntra";
+	public $gender = "Male";
 
-function __construct($title, $firstName, $lastName, $breed) {
+function __construct($title, $firstName, $lastName, $gender) {
 	$this>
 		firstName = $firstName;
 	$this>
 		lastName = $lastName;
 	$this>
-		breed = $breed;
+		gender = $gender;
 }
 function getName() {
-	return “{$this>
-	firstName}” .
+	return “{$this->firstName}” .
 	“{$this->lastName}”;
 }
 }
+$Person1 = new Person(“Austin”, “Juntra”, “Male”);
+print “Person 1: {$dog1->getName()}\n;
+class Cat {
+	public $firstName = "Tom";
+	public $lastName = "Baret";
+	public $gender = "Male";
+
+function __construct($title, $firstName, $lastName, $gender) {
+	$this>
+		firstName = $firstName;
+	$this>
+		lastName = $lastName;
+	$this>
+		gender = $gender;
+}
+function getName() {
+	return “{$this->firstName}” .
+	“{$this->lastName}”;
+}
+}
+$Cat1 = new Cat(“Tom”, “Baret”, “Male”);
+print “Cat 1: {$->getName()}\n;
+class Bat {
+	public $firstName = "Bat";
+	public $lastName = "Man";
+	public $gender = "Male";
+
+function __construct($title, $firstName, $lastName, $gender) {
+	$this>
+		firstName = $firstName;
+	$this>
+		lastName = $lastName;
+	$this>
+		gender = $gender;
+}
+function getName() {
+	return “{$this->firstName}” .
+	“{$this->lastName}”;
+}
+}
+$Bat1 = new Bat(“Tom”, “Baret”, “Male”);
+print “Bat 1: {$->getName()}\n;
